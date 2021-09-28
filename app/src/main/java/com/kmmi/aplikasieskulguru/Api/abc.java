d11e7bb31637b897f164a31497832dc91c819d76
MohammadFahrie <channel.mfahrie@gmail.com>
2021-09-28 23:43:48 +0700

    update terbaru

---
+++ app/src/main/java/com/kmmi/aplikasieskulguru/Api/abc.java
@@ -0,0 +1,9 @@
+package com.kmmi.aplikasieskulguru.Api;
+
+import android.widget.Toast;
+
+public class abc {
+
+    private String abcd;
+    private String defg;
+}
---
+++ app/src/main/java/com/kmmi/aplikasieskulguru/Model/PengumumanModel.java
@@ -0,0 +1,59 @@
+package com.kmmi.aplikasieskulguru.Model;
+
+public class PengumumanModel {
+
+    private int idpengumuman;
+    private String judulPengumuman;
+    private String isiPengumuman;
+    private String status;
+    private String fotoPengumuman;
+    private int ideskul;
+
+    public int getIdpengumuman() {
+        return idpengumuman;
+    }
+
+    public void setIdpengumuman(int idpengumuman) {
+        this.idpengumuman = idpengumuman;
+    }
+
+    public String getJudulPengumuman() {
+        return judulPengumuman;
+    }
+
+    public void setJudulPengumuman(String judulPengumuman) {
+        this.judulPengumuman = judulPengumuman;
+    }
+
+    public String getIsiPengumuman() {
+        return isiPengumuman;
+    }
+
+    public void setIsiPengumuman(String isiPengumuman) {
+        this.isiPengumuman = isiPengumuman;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFotoPengumuman() {
+        return fotoPengumuman;
+    }
+
+    public void setFotoPengumuman(String fotoPengumuman) {
+        this.fotoPengumuman = fotoPengumuman;
+    }
+
+    public int getIdeskul() {
+        return ideskul;
+    }
+
+    public void setIdeskul(int ideskul) {
+        this.ideskul = ideskul;
+    }
+}
